c3cd3c9273f336e9823c92d69616f6ac0aad0f91
Austin Isakson <austinjisakson@gmail.com>
2020-10-09 05:10:38 -0700

    Cleaned up MainScreenController and AppointmentScreenController to reflect modified appointment table. Main Screen now displaying upcoming and previous appointments properly.

--- src/main/java/austinisakson/consultcalmaven/MainScreenController.java
+++ src/main/java/austinisakson/consultcalmaven/MainScreenController.java
@@ -51,27 +51,34 @@ public class MainScreenController implements Initializable {
     @FXML
     final ToggleGroup group = new ToggleGroup();
     @FXML
-    RadioButton thisWeek = new RadioButton();        
+    RadioButton upcoming = new RadioButton();        
     @FXML
-    RadioButton thisMonth = new RadioButton();
+    RadioButton previous = new RadioButton();
     @FXML
-    Button manageCustomers = new Button();
+    Button manageClients = new Button();
     @FXML
     Button manageAppts = new Button();
     @FXML
     Button reports = new Button();
     @FXML
+    Button adminPanel = new Button();
+    @FXML
+    Button logoutButton = new Button();
+    @FXML
+    Button view = new Button();
+    
+    @FXML
     private ObservableList<Appointment> data = FXCollections.observableArrayList();
     @FXML
     private ObservableList<Appointment> allUpcoming = FXCollections.observableArrayList();
     @FXML
-    private TableColumn<Appointment, Integer> column1 = new TableColumn<>("Title");
+    private TableColumn<Appointment, Calendar> column1 = new TableColumn<>("Start Time");
     @FXML
-    private TableColumn<Appointment, Calendar> column2 = new TableColumn<>("Start Time");
+    private TableColumn<Appointment, Calendar> column2 = new TableColumn<>("End Time");
     @FXML
-    private TableColumn<Appointment, String> column3 = new TableColumn<>("Location");
+    private TableColumn<Appointment, String> column3 = new TableColumn<>("Contact");
     @FXML
-    private TableColumn<Appointment, String> column4 = new TableColumn<>("Contact");
+    private TableColumn<Appointment, String> column4 = new TableColumn<>("Location");
     @FXML
     private TableView appointmentView;
     
@@ -89,33 +96,33 @@ public class MainScreenController implements Initializable {
         LocalDate localDate = LocalDateTime.ofInstant(cal.toInstant(), zid).toLocalDate();
         return localDate;
     }
-   
-    private void alert15Min(ObservableList<Appointment> allUpcoming){
-        for (Appointment appointment : allUpcoming){
-            
-            Appointment appointment2 = appointment;
-            appointment2.getStart().add(MINUTE, -15);
-            
-            GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar.getInstance();
-            
-            if ((calendar.after(appointment2.getStart())) && (appointment.getUserID() == LoginScreenController.currentUserId)){
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Upcoming Appointment");
-                alert.setHeaderText("You have an appointment starting soon.");
-                alert.setContentText("Your calendar shows an appointment beginning within the next 15 minutes!");
-                alert.showAndWait();
-                System.out.println("Upcoming appointment alert.");
-                
-                break;
-            }
-        }
-    }
     
+    @FXML
+    private void handleMainScreen(ActionEvent event) throws IOException {
+        
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/MainScreen.fxml"));
+        Scene mainScene = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
     
     @FXML
     private void handleManageAppointments(ActionEvent event) throws IOException {
         
-        Parent mainParent = FXMLLoader.load(getClass().getResource("AppointmentScreen.fxml"));
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/AppointmentScreen.fxml"));
+        Scene mainScene = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
+    
+    @FXML
+    private void handleManageClients(ActionEvent event) throws IOException {
+        
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/ClientScreen.fxml"));
         Scene mainScene = new Scene(mainParent);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
@@ -126,7 +133,30 @@ public class MainScreenController implements Initializable {
     @FXML
     private void handleReports(ActionEvent event) throws IOException {
 
-        Parent mainParent = FXMLLoader.load(getClass().getResource("ReportScreen.fxml"));
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/ReportScreen.fxml"));
+        Scene mainScene = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
+    
+    @FXML
+    private void handleAdminPanel(ActionEvent event) throws IOException {
+        
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/MainScreen.fxml"));
+        Scene mainScene = new Scene(mainParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
+    
+    @FXML
+    private void handleLogout(ActionEvent event) throws IOException {
+        
+        LoginScreenController.currentUserId = 0;
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/LoginScreen.fxml"));
         Scene mainScene = new Scene(mainParent);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
@@ -138,36 +168,34 @@ public class MainScreenController implements Initializable {
     private void handleGenerate(ActionEvent event) throws IOException {
         
         // if radio selected this week, generate all of this week's appointments in the table view
-        if (thisWeek.isSelected()){
+        if (upcoming.isSelected()){
             try {
                 appointmentView.getItems().clear();
                 // Query result set for Appointment Table, load each appointment as an object of the Appointment class, display as a TableView
-                ResultSet appointmentTable = conn.createStatement().executeQuery("SELECT * FROM appointment WHERE start >= NOW() AND start < NOW() + INTERVAL 1 WEEK");
+                ResultSet appointmentTable = conn.createStatement().executeQuery("SELECT * FROM appointment WHERE starttime >= NOW();");
                 while (appointmentTable.next()){
                     Appointment newAppointment = new Appointment();
-                    newAppointment.setAppointmentID(appointmentTable.getInt("appointmentid"));
-                    newAppointment.setCustomerID(appointmentTable.getInt("customerId"));
-                    newAppointment.setUserID(appointmentTable.getInt("userID"));
-                    newAppointment.setTitle(appointmentTable.getString("title"));
-                    newAppointment.setDescription(appointmentTable.getString("description"));
+                    newAppointment.setAppointmentID(appointmentTable.getInt("id"));
+                    newAppointment.setClientID(appointmentTable.getInt("client"));
+                    newAppointment.setSchedulerID(appointmentTable.getInt("scheduler"));
+                    newAppointment.setDetails(appointmentTable.getString("details"));
                     newAppointment.setLocation(appointmentTable.getString("location"));
                     newAppointment.setContact(appointmentTable.getString("contact"));
-                    newAppointment.setType(appointmentTable.getString("type"));
-                    newAppointment.setURL(appointmentTable.getString("url"));
                     
-                    Timestamp startStamp = appointmentTable.getTimestamp("start");
+                    Timestamp startStamp = appointmentTable.getTimestamp("starttime");
                     GregorianCalendar startCal = (GregorianCalendar) GregorianCalendar.getInstance();
                     startCal.setTimeInMillis(startStamp.getTime());
                     newAppointment.setStart(startCal);
-                    Timestamp endStamp = appointmentTable.getTimestamp("end");
+                    Timestamp endStamp = appointmentTable.getTimestamp("endtime");
                     GregorianCalendar endCal = (GregorianCalendar) GregorianCalendar.getInstance();
                     endCal.setTimeInMillis(endStamp.getTime());
                     newAppointment.setEnd(endCal);
                     
-                    newAppointment.setCreatedDate(appointmentTable.getDate("createDate"));
+                    newAppointment.setCreatedDate(appointmentTable.getTimestamp("createdDate"));
                     newAppointment.setCreatedBy(appointmentTable.getString("createdBy"));
                     newAppointment.setLastUpdate(appointmentTable.getTimestamp("lastUpdate"));
-                    newAppointment.setLastUpdateBy(appointmentTable.getString("lastUpdateBy"));
+                    newAppointment.setLastUpdateBy(appointmentTable.getString("updatedBy"));
+                    newAppointment.setCompleted(appointmentTable.getBoolean("completed"));
                     data.add(newAppointment);
                     System.out.println("New appointment added from database!");
                 }
@@ -176,41 +204,39 @@ public class MainScreenController implements Initializable {
             
             }
             catch (SQLException ex) {
-                Logger.getLogger(CustomerScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(ClientScreenController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         
         // if radio selected this month, generate all of this month's appointments in the table view
-        else if (thisMonth.isSelected()){
+        else if (previous.isSelected()){
             try {
                 appointmentView.getItems().clear();
                 // Query result set for Appointment Table, load each appointment as an object of the Appointment class, display as a TableView
-                ResultSet appointmentTable = conn.createStatement().executeQuery("SELECT * FROM appointment WHERE start >= NOW() AND start < NOW() + INTERVAL 1 MONTH");
+                ResultSet appointmentTable = conn.createStatement().executeQuery("SELECT * FROM appointment WHERE starttime < NOW();");
                 while (appointmentTable.next()){
                     Appointment newAppointment = new Appointment();
-                    newAppointment.setAppointmentID(appointmentTable.getInt("appointmentid"));
-                    newAppointment.setCustomerID(appointmentTable.getInt("customerId"));
-                    newAppointment.setUserID(appointmentTable.getInt("userID"));
-                    newAppointment.setTitle(appointmentTable.getString("title"));
-                    newAppointment.setDescription(appointmentTable.getString("description"));
+                    newAppointment.setAppointmentID(appointmentTable.getInt("id"));
+                    newAppointment.setClientID(appointmentTable.getInt("client"));
+                    newAppointment.setSchedulerID(appointmentTable.getInt("scheduler"));
+                    newAppointment.setDetails(appointmentTable.getString("details"));
                     newAppointment.setLocation(appointmentTable.getString("location"));
                     newAppointment.setContact(appointmentTable.getString("contact"));
-                    newAppointment.setType(appointmentTable.getString("type"));
-                    newAppointment.setURL(appointmentTable.getString("url"));
                     
-                    Timestamp startStamp = appointmentTable.getTimestamp("start");
+                    Timestamp startStamp = appointmentTable.getTimestamp("starttime");
                     GregorianCalendar startCal = (GregorianCalendar) GregorianCalendar.getInstance();
                     startCal.setTimeInMillis(startStamp.getTime());
                     newAppointment.setStart(startCal);
-                    Timestamp endStamp = appointmentTable.getTimestamp("end");
+                    Timestamp endStamp = appointmentTable.getTimestamp("endtime");
                     GregorianCalendar endCal = (GregorianCalendar) GregorianCalendar.getInstance();
                     endCal.setTimeInMillis(endStamp.getTime());
                     newAppointment.setEnd(endCal);
                     
-                    newAppointment.setCreatedDate(appointmentTable.getDate("createDate"));
+                    newAppointment.setCreatedDate(appointmentTable.getTimestamp("createdDate"));
                     newAppointment.setCreatedBy(appointmentTable.getString("createdBy"));
                     newAppointment.setLastUpdate(appointmentTable.getTimestamp("lastUpdate"));
-                    newAppointment.setLastUpdateBy(appointmentTable.getString("lastUpdateBy"));
+                    newAppointment.setLastUpdateBy(appointmentTable.getString("updatedBy"));
+                    newAppointment.setCompleted(appointmentTable.getBoolean("completed"));
                     data.add(newAppointment);
                     System.out.println("New appointment added from database!");
                 }
@@ -219,7 +245,7 @@ public class MainScreenController implements Initializable {
             
             }
             catch (SQLException ex) {
-                Logger.getLogger(CustomerScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(ClientScreenController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -230,9 +256,11 @@ public class MainScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        manageCustomers.setOnAction((event)->  { // lambda expression here is used to remove a separately declared function and cleanly insert it into the initial program, eliminating the need to call a separate function
+        
+        // navigate to client screen when clicking Clients button
+        manageClients.setOnAction((event)->  { // lambda expression here is used to remove a separately declared function and cleanly insert it into the initial program, eliminating the need to call a separate function
             try {
-                Parent mainParent = FXMLLoader.load(getClass().getResource("CustomerScreen.fxml"));
+                Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/ClientScreen.fxml"));
                 Scene mainScene = new Scene(mainParent);
                 Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 
@@ -243,9 +271,21 @@ public class MainScreenController implements Initializable {
             }
         });
         
-        column1.setCellValueFactory(new PropertyValueFactory<>("title"));
-        column2.setCellValueFactory(new PropertyValueFactory<Appointment, Calendar>("start"));
+        // set up appointment table for display
         final DateFormat dateFormat = DateFormat.getInstance();
+        column1.setCellValueFactory(new PropertyValueFactory<>("start"));
+        column1.setCellFactory(col -> new TableCell<Appointment, Calendar>(){ // this lambda expression allows the anonymous function "updateItem" to insert its content as the parameter for the setCellFactory method
+            @Override
+            public void updateItem(Calendar item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null) {
+                    setText(null);
+                } else {
+                    setText(dateFormat.format(item.getTime()));
+                }
+            }
+        });
+        column2.setCellValueFactory(new PropertyValueFactory<>("end"));
         column2.setCellFactory(col -> new TableCell<Appointment, Calendar>(){ // this lambda expression allows the anonymous function "updateItem" to insert its content as the parameter for the setCellFactory method
             @Override
             public void updateItem(Calendar item, boolean empty) {
@@ -257,54 +297,11 @@ public class MainScreenController implements Initializable {
                 }
             }
         });
-        column3.setCellValueFactory(new PropertyValueFactory<>("location"));
-        column4.setCellValueFactory(new PropertyValueFactory<>("contact"));
-        
-        // generate and populate current date
-        
-        // generate and populate local time
-        
-        thisWeek.setToggleGroup(group);
-        thisMonth.setToggleGroup(group);
-        
-        try {
-            // Query result set for Appointment Table, load each appointment as an object of the Appointment class, display as a TableView
-            ResultSet appointmentTable = conn.createStatement().executeQuery("SELECT * FROM appointment WHERE start >= NOW()");
-            while (appointmentTable.next()){
-                Appointment newAppointment = new Appointment();
-                newAppointment.setAppointmentID(appointmentTable.getInt("appointmentid"));
-                newAppointment.setCustomerID(appointmentTable.getInt("customerId"));
-                newAppointment.setUserID(appointmentTable.getInt("userID"));
-                newAppointment.setTitle(appointmentTable.getString("title"));
-                newAppointment.setDescription(appointmentTable.getString("description"));
-                newAppointment.setLocation(appointmentTable.getString("location"));
-                newAppointment.setContact(appointmentTable.getString("contact"));
-                newAppointment.setType(appointmentTable.getString("type"));
-                newAppointment.setURL(appointmentTable.getString("url"));
+        column3.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        column4.setCellValueFactory(new PropertyValueFactory<>("location"));
 
-                Timestamp startStamp = appointmentTable.getTimestamp("start");
-                GregorianCalendar startCal = (GregorianCalendar) GregorianCalendar.getInstance();
-                startCal.setTimeInMillis(startStamp.getTime());
-                newAppointment.setStart(startCal);
-                Timestamp endStamp = appointmentTable.getTimestamp("end");
-                GregorianCalendar endCal = (GregorianCalendar) GregorianCalendar.getInstance();
-                endCal.setTimeInMillis(endStamp.getTime());
-                newAppointment.setEnd(endCal);
-
-                newAppointment.setCreatedDate(appointmentTable.getDate("createDate"));
-                newAppointment.setCreatedBy(appointmentTable.getString("createdBy"));
-                newAppointment.setLastUpdate(appointmentTable.getTimestamp("lastUpdate"));
-                newAppointment.setLastUpdateBy(appointmentTable.getString("lastUpdateBy"));
-                allUpcoming.add(newAppointment);
-            }
-
-
-        }
-        catch (SQLException ex) {
-            Logger.getLogger(CustomerScreenController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        alert15Min(allUpcoming);
-}    
-    
+        // set radio toggle options
+        upcoming.setToggleGroup(group);
+        previous.setToggleGroup(group);
+    }
 }
