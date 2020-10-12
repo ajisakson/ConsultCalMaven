d5b813661692230d4b627f0905109dfe199c268c
Austin Isakson <austinjisakson@gmail.com>
2020-10-12 09:26:24 -0700

    Set up ChoiceBoxes in ApptDetails and made ApptDetails and ClientDetails accessible in a separate window.

--- src/main/java/austinisakson/consultcalmaven/ApptDetailsController.java
+++ src/main/java/austinisakson/consultcalmaven/ApptDetailsController.java
@@ -8,6 +8,10 @@ package austinisakson.consultcalmaven;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -30,9 +34,9 @@ public class ApptDetailsController implements Initializable {
     @FXML
     private DatePicker apptDate;
     @FXML
-    private ChoiceBox startTime;
+    private ChoiceBox<String> startTime = new ChoiceBox<String>();
     @FXML
-    private ChoiceBox endTime;
+    private ChoiceBox<String> endTime = new ChoiceBox<String>();
     @FXML
     private TextField location;
     @FXML
@@ -43,6 +47,18 @@ public class ApptDetailsController implements Initializable {
     private TextField contact;
     @FXML
     private CheckBox completed;
+
+    private ObservableList<String> times = FXCollections.observableArrayList(
+        "09:00", "09:15", "09:30", "09:45", 
+        "10:00", "10:15", "10:30", "10:45",
+        "11:00", "11:15", "11:30", "11:45", 
+        "12:00", "12:15", "12:30", "12:45", 
+        "13:00", "13:15", "13:30", "13:45", 
+        "14:00", "14:15", "14:30", "14:45", 
+        "15:00", "15:15", "15:30", "15:45", 
+        "16:00", "16:15", "16:30", "16:45", 
+        "17:00");
+    
     
     @FXML
     Button saveButton = new Button();
@@ -59,9 +75,7 @@ public class ApptDetailsController implements Initializable {
     Label lastUpdateLabel = new Label();
     @FXML
     Label updatedByLabel = new Label();
-    
-    
-    
+
     
     /**
      * Initializes the controller class.
@@ -69,6 +83,8 @@ public class ApptDetailsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        startTime.setItems(times);
+        endTime.setItems(times);
     }    
     
 }
